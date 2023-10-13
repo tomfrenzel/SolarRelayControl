56fc64c9de48cfbbbca6dd43dccd07b540e1d57b
Tom Frenzel <admin@tom-frenzel.de>
2023-10-13 15:23:07 +0200

    fix: invert

--- SolarRelayControl/Client/Program.cs
+++ SolarRelayControl/Client/Program.cs
@@ -5,6 +5,7 @@ using SolarRelayControl.Client;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
 builder.Services.AddLocalization();
 
 await builder.Build().RunAsync();
